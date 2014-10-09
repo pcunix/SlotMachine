90fe9d2cd1e9316d9f0b08636728cc8d8849dca7
Tony <tony@aplawrence.com>
2014-10-09 07:37:08 -0400

    no labels

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -26,6 +26,15 @@ class ViewController: UIViewController {
     
     //Top Label
     var titleLabel:UILabel!
+    
+    //Info labels
+    
+    var creditsLabel: UILabel!
+    var betLabel: UILabel!
+    var winnerPaidLabel: UILabel!
+    var creditsTitleLabel: UILabel!
+    var betTitleLabel: UILabel!
+    var winnerPaidTitleLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +42,7 @@ class ViewController: UIViewController {
         setupContainerViews()
         setupFirstContainer(self.firstContainer)
         setupSecondContainer(self.secondContainer)
+        setupThirdContainer(self.thirdContainer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -76,6 +86,18 @@ class ViewController: UIViewController {
                 containerView.addSubview(slotImageView)
             }
         }
+    }
+    
+    func setupThirdContainer(containerView:UIView) {
+        self.creditsLabel = UILabel()
+        self.creditsLabel.text = "000000"
+        self.creditsLabel.textColor = UIColor.redColor()
+        self.creditsLabel.font = UIFont(name: "Menlo-Bold", size: 16)
+        self.creditsLabel.sizeToFit()
+        self.creditsLabel.center = CGPointMake(containerView.frame.width * kSixth, containerView.frame.height * kThird)
+        self.creditsLabel.textAlignment = NSTextAlignment.Center
+        self.creditsLabel.backgroundColor = UIColor.darkGrayColor()
+        containerView.addSubview(self.creditsLabel)
     }
 }
 
